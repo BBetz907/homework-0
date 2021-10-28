d6bf730140d738984e5a09273de732e1c153ecee
Bourke Betz <BBETZ@USAID.GOV>
2021-10-28 09:19:09 -0700

    updating again

--- code.r
+++ code.r
@@ -1,3 +1,11 @@
 library(tidyverse)
 library(dplyr)
-#only required packages
+#only required packagesgit
+
+install.packages("usethis")
+library(usethis)
+
+use_git_config(user.name = "Bourke Betz", user.email = "bourke.betz@gmail.com")
+
+
+edit_git_config()
