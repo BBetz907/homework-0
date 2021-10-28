060fd2357476a7d9ad99aaf37e0fcbbf8fef1971
Bourke Betz <BBETZ@USAID.GOV>
2021-10-28 09:21:40 -0700

    ready to go

--- code.r
+++ code.r
@@ -1,11 +1,4 @@
 library(tidyverse)
 library(dplyr)
-#only required packagesgit
-
-install.packages("usethis")
-library(usethis)
-
-use_git_config(user.name = "Bourke Betz", user.email = "bourke.betz@gmail.com")
-
-
-edit_git_config()
+library(ggplot2)
+#all set
